702e051b0b41384828fc1a48b7f0c5d8c31762ee
cefariron <adidaswww17@gmail.com>
2023-11-27 12:07:49 +0200

    complete task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.body;
 
+btnStart.addEventListener('click', handleStart);
+btnStop.addEventListener("click", handleStop)
+
+let intervalId;
+btnStop.setAttribute('disabled', true);
+
+function handleStart() {
+  body.style.backgroundColor = getRandomHexColor();
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.setAttribute('disabled', true);
+  btnStop.disabled = false;
+}
+
+function handleStop() {
+    btnStart.disabled = false;
+    clearInterval(intervalId);
+    btnStop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
